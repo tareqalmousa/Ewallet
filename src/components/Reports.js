4799b3174b1169fc3fe97815581315676912b5ea
tareqalmousa <tarek.almousa@sla-ltd.com>
2021-06-10 19:41:38 +0800

    add chart component

--- src/components/Reports.js
+++ src/components/Reports.js
@@ -1,6 +1,33 @@
 import Card from "./Card";
+import Chart from "react-apexcharts";
 
+const chartRender = (data) => {
+    let series = [];
+    let labels = [];
+    Object.entries(data).map(regregord => (
+        series.push(regregord[1].value),
+            labels.push(regregord[1].label)
+    ));
+    let chartRetrun = {
+        series: series,
+        options: {
+            chart: {
+                width: 380,
+                type: 'pie',
+            },
+            labels: labels,
+            legend: {
+                position: 'bottom',
+            }
+        },
+    };
+    return chartRetrun;
+};
 const Reports = ({data}) => {
+    const account_frozen = chartRender(data.exceptions);
+    const total_transaction_amount = chartRender(data.total_transaction_amount);
+    const total_transaction_count = chartRender(data.total_transaction_count);
+
     return (
         <div className="container pt-3">
             <div className="card-cover">
@@ -16,6 +43,34 @@ const Reports = ({data}) => {
                         <Card color={'blue'} footer={'CASH out average'} body={data.cash_out_average}/>
                         <Card color={'blue'} footer={'CASH out maximum'} body={data.cash_out_maximum}/>
                         <Card color={'blue'} footer={'CASH out minimum'} body={data.cash_out_minimum}/>
+                    </div>
+                </div>
+            </div>
+            <div className="container pt-3">
+                <div className="card-cover">
+                    <div className="container pt-3">
+                        <div className="row">
+                            <div className="col">
+                                <Chart
+                                    options={account_frozen.options}
+                                    series={account_frozen.series}
+                                    type="pie"
+                                    width="320"
+                                />
+                            </div>
+                            <div className="col"> <Chart
+                                options={total_transaction_amount.options}
+                                series={total_transaction_amount.series}
+                                type="pie"
+                                width="320"
+                            /></div>
+                            <div className="col"> <Chart
+                                options={total_transaction_count.options}
+                                series={total_transaction_count.series}
+                                type="pie"
+                                width="320"
+                            /></div>
+                        </div>
                     </div>
                 </div>
             </div>
